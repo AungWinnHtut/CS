2b9404d284a9110cbff06ca45d90e87e43c578da
Dr. Aung Win Htut <aungwinnhtut@gmail.com>
2023-12-24 20:37:43 +0630

    branching and for looping examples

---
+++ pythonCS2/for.py
@@ -0,0 +1,6 @@
+radius = [12.2,11.4,14.0,16,22,324,2345,345,345,3454355436,546,356,67]
+
+for r in radius:
+    a = 3.14 * r * r
+    print(f'for r = {r} cm, area = {a} sq cm')
+
--- pythonCS2/read_n_calculate.py
+++ pythonCS2/read_n_calculate.py
@@ -6,4 +6,17 @@ r = float(r)
 ic(r)
 
 a = 3.14 * r * r
+
+if a <= 100:
+    print('Small Area')
+    print('.')
+elif a >100 and a <= 200:
+    print("Medium Area")
+elif a != 300:
+    pass
+else:
+    print('Large Area')
+
+
+
 print(f'A = {a}')
